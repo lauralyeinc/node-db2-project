5d6dbdacf2984452d4da9d236d9de420f6b7438a
Laura <theimer.laura@gmail.com>
2020-02-04 21:38:16 -0500

    MVP

--- cars/carsRouter.js
+++ cars/carsRouter.js
@@ -4,6 +4,8 @@ const carsDB = require('../data/dbConfig.js');
 
 const router = express.Router();
 
+
+//√√ 
 router.get('/', async (req, res) => {
     try {
         const cars = await carsDB('cars');
@@ -11,6 +13,31 @@ router.get('/', async (req, res) => {
     } catch(error) {
         console.log(error);
         res.status(500).json({ message: 'Failed to get cars'});
+    }
+});
+
+// √√ 
+router.get('/:id', async (req, res) => {
+    const {id} = req.params;
+    try {
+        const [car] = await carsDB('cars').where('id', id);
+        res.json(car);
+    } catch(err) {
+        console.log(err);
+        res.sendStatus(500).json({message: 'Failed to get car'})
+    }
+});
+
+//
+router.post('/', async (req, res) => {
+    const carData = req.body;
+    try {
+        const car = await carsDB('cars').insert(carData);
+        res.status(201).json(car);
+    }
+    catch(err) {
+        console.log(err);
+        res.sendStatus(500).json({ message: 'Unable to post new car'});
     }
 });
 
